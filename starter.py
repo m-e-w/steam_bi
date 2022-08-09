d407331c7fc0f5d7403752fa3e4595753c9e1fe6
m-e-w <walls.e.matthew@gmail.com>
2022-08-09 00:02:48 -0400

    Refactoring / added flags to get data in config file

--- starter.py
+++ starter.py
@@ -4,74 +4,50 @@ import yaml
 
 
 def main():
-    # Open the config file
     with open('config.yaml', 'r') as cfg:
         config = yaml.safe_load(cfg)
 
-    # Get config data
     steam = config['steam']
     options = config['options']
-
+    steam_ids = [steam['steamid']]
+    root_games = []
+    root_friends = []
     steam_api = SteamAPI(key=steam['key'], debug=options['debug'])
 
-    # Get games and friends of the root user
-    my_games = steam_api.get_owned_games(
-        steamId=steam['steamid'], include_store_data=False, store_limit=10, time_to_sleep=2)
-    my_friends = steam_api.get_friend_list(
-        steamId=steam['steamid'], include_player_summaries=False)
+    if(steam['data']['get_games']):
+        root_games = steam_api.get_owned_games(steamId=steam['steamid'], include_store_data=False, store_limit=10, time_to_sleep=2)
 
-    # List to hold all steamids (We will later take all steamids, de-dupe them, and generate a guid per each steamid to facilitate data anonymization)
-    steam_ids = []
+    if(steam['data']['get_friends']):
+        root_friends = steam_api.get_friend_list(steamId=steam['steamid'], include_player_summaries=False)
+        for friend in root_friends:
+            steam_ids.append(friend['steamid'])
 
-    # Add the root users steamid to our list of ids
-    steam_ids.append(steam['steamid'])
+    if(steam['data']['traverse_friends']):
+        for friend in root_friends:
+            games = steam_api.get_owned_games(steamId=friend['steamid'], include_store_data=False, store_limit=10, time_to_sleep=2)
+            friends = steam_api.get_friend_list(steamId=friend['steamid'], include_player_summaries=False)
+            friend['games'] = games
+            friend['friends'] = friends
+            for f in friends:
+                steam_ids.append(f['steamid'])
 
-    # Loop through each friend and grab their games and friends
-    for friend in my_friends:
-        # Add the friends steamid to our list of ids
-        steam_ids.append(friend['steamid'])
-        games = steam_api.get_owned_games(
-            steamId=friend['steamid'], include_store_data=False, store_limit=10, time_to_sleep=2)
-        friends = steam_api.get_friend_list(
-            steamId=friend['steamid'], include_player_summaries=False)
-
-        # Loop through all of their friends and add their friends ids to the list
-        for f in friends:
-            steam_ids.append(f['steamid'])
-
-        # Save their games / friends
-        friend['games'] = games
-        friend['friends'] = friends
-
-    steam_helper = SteamHelper(
-        debug=options['debug'], counter=steam_api.get_counter())
-
-    # Pass the de-duped list of steamids, and retrieve a list of dicts containing the steamid/guid
+    steam_helper = SteamHelper(debug=options['debug'], counter=steam_api.get_counter())
+    
     guids = steam_helper.generate_guids(id_set=set(steam_ids))
+    root_guid = steam_helper.find_match(records=guids, key='steamid', value=steam['steamid'])['guid']
+    steam_helper.replace_values(source_records=root_friends, target_records=guids, source_key='steamid', target_key='guid', sub_list_key='friends')
 
-    # Replace all steamids with a guid
-    steam_helper.replace_values(
-        source_records=my_friends, target_records=guids, source_key='steamid', target_key='guid')
-
-    # Look for our own matching guid
-    match = steam_helper.find_match(
-        records=guids, key='steamid', value=steam['steamid'])
-
-    # Build the dataset
     player = {
-        'steamid': match['guid'],
-        'games': my_games,
-        'friends': my_friends
+        'steamid': root_guid,
+        'games': root_games,
+        'friends': root_friends
     }
     data = {
         'player': player,
     }
 
-    # Write the dataset and the list of guids/steamids (We save this so we can un-anonymize the data later on if we want)
-    steam_helper.write_data(
-        data=data, file_path='data/steam_bi_data_extract.json')
-    steam_helper.write_data(
-        data=guids, file_path='data/steam_bi_steamid_guid.json')
+    steam_helper.write_data(data=data, file_path='data/steam_bi_data_extract.json')
+    steam_helper.write_data(data=guids, file_path='data/steam_bi_steamid_guid.json')
 
 
 if __name__ == '__main__':
--- steam_helper.py
+++ steam_helper.py
@@ -4,18 +4,32 @@ import os
 import json
 
 
-# Class to hold a few helper functions to work with the data returned from Steams API
 class SteamHelper:
+    """
+    Class to hold a few helper functions to work with the data returned from Steams API
+
+    :param str counter: Operations counter for logging
+    """
     def __init__(self, debug: bool, counter: int):
         self.debug = debug
         self.counter = counter
 
-    # Search a list of dictionaries by key:value. Return the matching dictionary.
     def find_match(self, records: list[dict], key: str, value: str):
+        """
+        Search a list of dictionaries by key:value. Return the matching dictionary
+
+        :param list[dict] records: Records to search
+        :param str key: Key to search for
+        :param str value: Value to match on
+        """
         return next(record for record in records if record[key] == value)
 
-    # Takes a set of steamids and generates a guid per each item. Returns a list of dictionaries containing the original steamid and the new guid
     def generate_guids(self, id_set: list):
+        """
+        Takes a set of steamids and generates a guid per each item. Returns a list of dictionaries containing the original steamid and the new guid
+
+        :param list[dict] id_set: A set of steamids
+        """
         items = []
         for id in id_set:
             item = {
@@ -25,24 +39,36 @@ class SteamHelper:
             items.append(item)
         return items
 
-    # Replaces all values of source_key within source_records with the value of target_key from target_records
-    def replace_values(self, source_records: list[dict], target_records: list[dict], source_key: str, target_key: str):
-        for record in source_records:
-            match = self.find_match(
-                records=target_records, key=source_key, value=record[source_key])
-            record[source_key] = match[target_key]
-            for friend in record['friends']:
-                match = self.find_match(
-                    records=target_records, key=source_key, value=friend[source_key])
-                friend['steamid'] = match[target_key]
+    def replace_values(self, source_records: list[dict], target_records: list[dict], source_key: str, target_key: str, sub_list_key: str):
+        """
+        Replaces all values of source_key within source_records with the value of target_key from target_records
 
-    # Takes a list of dictionaries and writes it to a json file
+        :param list[dict] source_records: The source records whose key will be replaced
+        :param list[dict] target_records: The target records that are matched to
+        :param str source_key: The key whose value is to be replaced
+        :param str target_key: The key containing the value to use
+        :param str sub_list_key: If source_records contains another list to replace records in, specify it here
+        """
+        for record in source_records:
+            match = self.find_match(records=target_records, key=source_key, value=record[source_key])
+            record[source_key] = match[target_key]
+
+            items = record.get(sub_list_key, [])
+            for item in items:
+                match = self.find_match(records=target_records, key=source_key, value=item[source_key])
+                item[source_key] = match[target_key]
+
     def write_data(self, file_path: str, data: list[dict]):
+        """
+        Takes a list of dictionaries and writes it to a json file
+
+        :param str file_path: The path to write the file
+        :param list[dict] data: The list of dictionaries to be write to disk
+        """
         self.counter = self.counter + 1
         if(file_path):
             if(self.debug):
-                print("[%s]\t%s\tWRITE\t%s" % (self.counter,
-                      datetime.now(), os.path.abspath(file_path)))
+                print("[%s]\t%s\tWRITE\t%s" % (self.counter, datetime.now(), os.path.abspath(file_path)))
 
             with open(file_path, 'w') as f:
                 json.dump(data, f)
