83fb5ddac26783619a00ae074c5c6e09757655ec
m-e-w <walls.e.matthew@gmail.com>
2022-08-23 12:07:16 -0400

    Renamed python/ to flask/

---
+++ flask/starter.py
@@ -0,0 +1,162 @@
+from __future__ import print_function
+from steam import SteamAPI
+from steam_helper import SteamHelper
+from datetime import datetime
+from faker import Faker
+import yaml
+import mysql.connector
+import logging
+
+def main():
+    print("%s\tSTART" %(datetime.now()))
+    with open('config.yaml', 'r') as cfg:
+        config = yaml.safe_load(cfg)
+
+    steam = config['steam']
+    mysql_cfg = config['mysql']
+    options = config['options']
+    debug = options['debug']
+    discover_friends = options['discover_friends']
+    discover_games = options['discover_games']
+    traverse_friends = options['traverse_friends']
+
+    if(debug):
+        logging.basicConfig(level=logging.DEBUG, filename='steam_bi.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    else:
+        logging.basicConfig(level=logging.ERROR, filename='steam_bi.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
+    insert_game = ("""
+        INSERT INTO game (app_id, name) VALUES (%(app_id)s, %(name)s)
+        ON DUPLICATE KEY UPDATE name=%(name)s
+    """)
+    insert_gameinuse = ("""
+        INSERT INTO gameinuse (playtime_forever_minutes, playtime_2weeks_minutes, user_fk, game_fk) VALUES (%(playtime_forever_minutes)s, %(playtime_2weeks_minutes)s, (SELECT user_pk FROM user WHERE steam_id=%(steam_id)s), (SELECT game_pk FROM game WHERE app_id=%(app_id)s))
+        ON DUPLICATE KEY UPDATE playtime_forever_minutes=%(playtime_forever_minutes)s, playtime_2weeks_minutes=%(playtime_2weeks_minutes)s
+    """)
+    insert_user = ("""
+        INSERT INTO user (steam_id, display_name) VALUES (%(steam_id)s, %(display_name)s)
+        ON DUPLICATE KEY UPDATE display_name=%(display_name)s
+    """)
+    query_game = "SELECT app_id from game"
+
+    steam_api = SteamAPI(key=steam['key'])
+    steam_ids = [steam['steamid']]
+
+    view_friends = []
+    view_gamesinuse = []
+
+    print("%s\tGathering data from steam ..." % (datetime.now()))
+    if (discover_friends or traverse_friends):
+        friend_list = steam_api.get_friend_list(
+            steamId=steam['steamid'], include_player_summaries=False)
+        if (friend_list):
+            for friend in friend_list:
+                steam_ids.append(friend['steamid'])
+                friend.update({'user_fk': steam['steamid']})
+            view_friends = friend_list
+
+    if (traverse_friends):
+        id_list = steam_ids.copy()
+        for steam_id in id_list:
+            if (discover_games):
+                games = []
+                games = steam_api.get_owned_games(
+                    steam_id, include_store_data=False)
+                if (games):
+                    for game in games:
+                        game.update({'steamid': steam_id})
+                    view_gamesinuse.extend(games)
+            if (discover_friends):
+                friends = []
+                friends = steam_api.get_friend_list(
+                    steamId=steam_id, include_player_summaries=False)
+                if (friends):
+                    for friend in friends:
+                        steam_ids.append(friend['steamid'])
+                        friend.update({'user_fk': steam_id})
+                    view_friends.extend(friends)
+
+    else:
+        if (discover_games):
+            items = []
+            items = steam_api.get_owned_games(
+                steam_ids[0], include_store_data=False)
+            if (items):
+                for item in items:
+                    item.update({'steamid': steam_ids[0]})
+                view_gamesinuse.extend(items)
+    
+    steam_helper = SteamHelper(debug=options['debug'])
+    guids = steam_helper.generate_guids(id_set=set(steam_ids))
+    users = guids.copy()
+    fake = Faker()
+    for user in users:
+        display_name = fake.word(part_of_speech='adjective').capitalize() + ' ' + fake.word(part_of_speech='noun').capitalize() + ' #' + str(fake.pyint())
+        user.update({'display_name': display_name})
+    assert len(set(user['display_name'] for user in users)) == len(users)
+
+    if(options['destination'] == 'mysql'):
+        cnx = mysql.connector.connect(host=mysql_cfg['host'], user=mysql_cfg['user'], password=mysql_cfg['password'], database=mysql_cfg['database'], connect_timeout=mysql_cfg['connect_timeout'])
+        cursor = cnx.cursor(buffered=True, dictionary=True)
+
+        print("%s\tGathering data from MySQL ..." %(datetime.now()))
+        # Query MySQL for a list of all app IDs
+        if(debug):
+            logging.debug("%s\tQUERY\t%s" %(datetime.now(), query_game))
+        cursor.execute(query_game)
+        rows_game = cursor.fetchall()
+        game_ids = [row['app_id'] for row in rows_game]
+
+        print("%s\tWriting data to MySQL ..." %(datetime.now()))
+        for user in users:
+            data_user = {
+                'steam_id': user['steamid'],
+                'display_name': user['display_name']
+            }
+            if(debug):
+                logging.debug("INSERT\t%s -> steam_bi[user]" %(data_user))
+            cursor.execute(insert_user, data_user)
+
+        for game in view_gamesinuse:
+            if (int(game['appid']) not in game_ids):
+                game_ids.append(game['appid'])
+                data_game = {
+                    'app_id': game['appid'],
+                    'name': game['name']
+                }
+                if(debug):
+                    logging.debug("INSERT\t%s -> steam_bi[game]" %(data_game))
+                cursor.execute(insert_game, data_game)
+            data_gameinuse = {
+                'playtime_forever_minutes': game.get('playtime_forever', None),
+                'playtime_2weeks_minutes': game.get('playtime_2weeks', None),
+                'steam_id': game['steamid'],
+                'app_id': game['appid']
+            }
+            if(debug):
+                logging.debug("INSERT\t%s -> steam_bi[gameinuse]" %(data_gameinuse))
+            cursor.execute(insert_gameinuse, data_gameinuse)
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+
+    # Deprecated. Leaving for backwards compatability
+    elif(options['destination'] == 'json'):
+        for friend in view_friends:
+            user_fk = friend['user_fk']
+            friend.update({'user_fk': steam_helper.find_match(records=guids, key='steamid', value=user_fk)['guid']})
+        
+        steam_helper.replace_values(source_records=view_gamesinuse,target_records=guids, source_key='steamid', target_key='guid')
+        steam_helper.replace_values(source_records=view_friends,target_records=guids, source_key='steamid', target_key='guid')
+        steam_helper.write_data(data=guids, file_path='data/steam_bi_view_guids.json')
+        steam_helper.write_data(data=view_gamesinuse,file_path='data/steam_bi_view_gamesinuse.json')
+        steam_helper.write_data(data=view_friends,file_path='data/steam_bi_view_friends.json')
+
+        for user in users:
+            user.pop('steamid')
+        steam_helper.write_data(data=users, file_path='data/steam_bi_view_users.json')
+    
+    print("%s\tEND" %(datetime.now()))
+
+if __name__ == '__main__':
+    main()
---
+++ flask/steam_helper.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+import uuid
+import os
+import json
+
+
+class SteamHelper:
+    """
+    Class to hold a few helper functions to work with the data returned from Steams API
+    """
+    def __init__(self, debug: bool):
+        self.debug = debug
+
+    def find_match(self, records: list[dict], key: str, value: str):
+        """
+        Search a list of dictionaries by key:value. Return the matching dictionary
+
+        :param list[dict] records: Records to search
+        :param str key: Key to search for
+        :param str value: Value to match on
+        """
+        return next(record for record in records if record[key] == value)
+
+    def generate_guids(self, id_set: list):
+        """
+        Takes a set of steamids and generates a guid per each item. Returns a list of dictionaries containing the original steamid and the new guid
+
+        :param list[dict] id_set: A set of steamids
+        """
+        items = []
+        for id in id_set:
+            item = {
+                'guid': uuid.uuid4().hex,
+                'steamid': id
+            }
+            items.append(item)
+        return items
+
+    def replace_values(self, source_records: list[dict], target_records: list[dict], source_key: str, target_key: str, **kwargs):
+        """
+        Replaces all values of source_key within source_records with the value of target_key from target_records
+
+        :param list[dict] source_records: The source records whose key will be replaced
+        :param list[dict] target_records: The target records that are matched to
+        :param str source_key: The key whose value is to be replaced
+        :param str target_key: The key containing the value to use
+        :param str sub_list_key: (Optional) If source_records contains another list to replace records in, specify it here
+        """
+        sub_list_key = kwargs.get('sub_list_key', None)
+        for record in source_records:
+            match = self.find_match(records=target_records, key=source_key, value=record[source_key])
+            record[source_key] = match[target_key]
+
+            items = record.get(sub_list_key, [])
+            for item in items:
+                match = self.find_match(records=target_records, key=source_key, value=item[source_key])
+                item[source_key] = match[target_key]
+
+    def write_data(self, file_path: str, data: list[dict]):
+        """
+        Takes a list of dictionaries and writes it to a json file
+
+        :param str file_path: The path to write the file
+        :param list[dict] data: The list of dictionaries to be write to disk
+        """
+        if(file_path):
+            if(self.debug):
+                print("%s\tWRITE\t%s" % (datetime.now(), os.path.abspath(file_path)))
+
+            with open(file_path, 'w') as f:
+                json.dump(data, f)
