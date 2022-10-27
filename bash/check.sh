5e603405f1c6498fc99cf6bd32689d4a887ae8b2
m-e-w <walls.e.matthew@gmail.com>
2022-10-27 19:28:00 -0400

    Print process group name before ps output

--- bash/check.sh
+++ bash/check.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 # Check for gunicorn processes
+printf "[gunicorn]\n"
 ps auxww | grep '/.virtual_envs/steam_bi_dev/bin/python3' | grep 'gunicorn' | grep -v ' grep '
 
 # Check for celery processes
+printf "[celery]\n"
 ps auxww | grep '/.virtual_envs/steam_bi_dev/bin/python3' | grep 'celery' | grep -v ' grep '
