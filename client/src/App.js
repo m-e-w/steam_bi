e9e4c6a1bd314d9a61e0b6efb6cd876733500152
NickPC <nicholas.giuliani94@gmail.com>
2023-01-20 19:13:30 -0500

    Started POC for connecting Flask API with react, will need more info about what port the Flask API is attached to

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const [data, setData] = useState([{}]);
+
+// Need to know what port the Flask API is attached to in order to finish POC
+useEffect(() => {
+  fetch("/something").then(
+    res => res.json()
+  ).then(
+    data => {
+      setData(data)
+      console.log(data)
+    }
+  )
+}, []);
+
+return (
+  <div>
+    {(typeof data === 'undefined') ? (
+        <p>Loading...</p>
+    ) : (
+      data.map((users, i) => (
+        <p key={i}>
+          {users}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      ))
+    )}
+
+  </div>
+)
 }
 
-export default App;
+export default App
