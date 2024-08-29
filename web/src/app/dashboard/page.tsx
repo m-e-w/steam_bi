f3f135b0f5b6d80b414ea6494759328b76c126b5
Nick <nicholas.giuliani94@gmail.com>
2024-08-28 22:43:58 -0400

    more hero updates

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      Dashboard
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
+        eaque non adipisci qui velit consequatur neque tenetur beatae eos porro.
+        Fugit aut numquam illo, totam tempora corrupti modi magni dolores.
+      </p>
+    </div>
+  );
 }
--- web/src/app/hero.tsx
+++ web/src/app/hero.tsx
@@ -4,11 +4,11 @@ import { Input } from "@/components/ui/input";
 
 export default function Hero() {
   return (
-    <section className="w-full pt-32 border-b">
+    <section className="w-full pt-32">
       <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
-        <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
+        <div className="mx-auto grid max-w-[750px] gap-4 px-4 sm:px-6 md:px-10 md:gap-16">
           <div>
-            <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+            <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter pb-10 sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
               A Data Visualization tool for your Steam Account.
             </h1>
             <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
@@ -24,15 +24,7 @@ export default function Hero() {
               <Button type="submit">Submit</Button>
             </form>
           </div>
-          <div className="flex flex-col items-start space-y-4">
-            <img
-              src="/vercel.svg"
-              width="550"
-              height="310"
-              alt="Hero"
-              className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
-            />
-          </div>
+
         </div>
       </div>
     </section>
