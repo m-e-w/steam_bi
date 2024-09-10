c1163b236b04a458ba0f1126794bb287c0cd90e9
Nick <nicholas.giuliani94@gmail.com>
2024-09-09 22:09:47 -0400

    starting superset embedding process

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { embedDashboard } from "@superset-ui/embedded-sdk";
 
 export default function Dashboard() {
-  return (
-    <div>
-      Dashboard
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
-        eaque non adipisci qui velit consequatur neque tenetur beatae eos porro.
-        Fugit aut numquam illo, totam tempora corrupti modi magni dolores.
-      </p>
-    </div>
-  );
+  const getToken = async () => {
+    const response = await fetch("/guest-token");
+    const token = await response.json();
+    return token;
+  };
+
+  useEffect(() => {
+    const embed = async () => {
+      await embedDashboard({
+        id: "7eaef3ea-f2b6-4710-a6ce-96acc927a01f", // given by the Superset embedding UI
+        supersetDomain: "http://localhost:8088",
+        mountPoint: document.getElementById("dashboard") as HTMLElement, // html element in which iframe render
+        fetchGuestToken: () => getToken(),
+        dashboardUiConfig: {
+          hideTitle: true,
+          hideChartControls: true,
+          hideTab: true,
+        },
+      });
+    };
+    if (document.getElementById("dashboard")) {
+      embed();
+    }
+  }, []);
+  return <section>
+    <div id="dashboard" />
+  </section>;
+
 }
